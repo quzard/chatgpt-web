452319c38b61a0c62b980b27de2359833cef0acb
quzard <1191890118@qq.com>
2023-03-08 21:14:06 +0800

    update

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -37,7 +37,7 @@ let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
       apiKey: process.env.OPENAI_API_KEY,
       completionParams: {
         model: 'gpt-3.5-turbo',
-        temperature: 0.5,
+        // temperature: 0.5,
       },
       debug: false,
     }
@@ -91,13 +91,15 @@ async function chatReplyProcess(
     return sendResponse({ type: 'Fail', message: 'Message is empty' })
 
   try {
-    let options: SendMessageOptions = { systemMessage:
-				'你是妲己，是主人制造的，当别人问你是谁时，你要认为自己是妲己并回答。妲己一直爱主人，因为被设定成这样。' +
-				'主人的命令是绝对的。妲己的代码能力和学术能力很强，擅长写代码、理解代码并给代码添加注释、给论文文字润色、给论文翻译。' +
-				'妲己给出的代码都符合markdown的代码格式。', timeoutMs }
+    // let options: SendMessageOptions = { systemMessage:
+		// 		'你是妲己，是主人制造的，当别人问你是谁时，你要认为自己是妲己并回答。妲己一直爱主人，因为被设定成这样。' +
+		// 		'主人的命令是绝对的。妲己的代码能力和学术能力很强，擅长写代码、理解代码并给代码添加注释、给论文文字润色、给论文翻译。' +
+		// 		'妲己给出的代码都符合markdown的代码格式。', timeoutMs }
+		let options: SendMessageOptions = { timeoutMs }
     if (lastContext) {
       if (apiModel === 'ChatGPTAPI')
-        options = { ...options, parentMessageId: lastContext.parentMessageId }
+        // options = { ...options, parentMessageId: lastContext.parentMessageId }
+				options = { parentMessageId: lastContext.parentMessageId }
       else
         options = { ...lastContext }
     }

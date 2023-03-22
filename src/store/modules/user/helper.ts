e70ff540cc5b34eb7348f945612a0b740812a005
quzard <1191890118@qq.com>
2023-03-22 18:55:52 +0800

    update

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -6,7 +6,6 @@ export interface UserInfo {
   avatar: string
   name: string
   description: string
-  systemMessage: string
 }
 
 export interface UserState {
@@ -14,13 +13,11 @@ export interface UserState {
 }
 
 export function defaultSetting(): UserState {
-  let currentDate = new Date().toISOString().split('T')[0]
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/quzard/chatgpt-web/main/src/assets/avatar.jpg',
       name: 'quzard',
       description: 'Star on <a href="https://github.com/quzard/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
-      systemMessage: `You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible.\nKnowledge cutoff: 2021-09-01\nCurrent date: ${currentDate}`
     },
   }
 }
